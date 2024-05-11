2dd2a915b5a2dd51f31d769c3c139e343ef71cf8
tig3rmast3r <tigerman.email@gmail.com>
2024-05-11 15:10:55 +0200

    fixes2

--- scripts/utils/quickinstall.sh
+++ scripts/utils/quickinstall.sh
@@ -3,17 +3,19 @@
 # pth download not included, you will have to download manually
 # BEFORE launching this script:
 # cd workspace
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/Miniconda3-latest-Linux-x86_64.sh
+# bash /workspace/Miniconda3-latest-Linux-x86_64.sh -b -p /workspace/miniconda
+# source ~/.bashrc
+# conda init bash
+# source ~/.bashrc
+# conda create -n vampnet python=3.10.14 -y
+# conda activate vampnet
 # wget https://raw.githubusercontent.com/tig3rmast3r/vampnet/ismir/scripts/utils/quickinstall.sh
 # chmod +x quickinstall.sh
 # ./quickinstall.sh
 
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/Miniconda3-latest-Linux-x86_64.sh
-bash /workspace/Miniconda3-latest-Linux-x86_64.sh -b -p /workspace/miniconda
-export PATH="/workspace/miniconda/bin:$PATH"
-conda create -n vampnet python=3.10.14 -y
-export PATH="/workspace/miniconda/envs/vampnet/bin:$PATH"
 git clone https://github.com/hugofloresgarcia/vampnet.git
+#this is pytorch 2.3.0 at the time of writing
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
 pip install cython
 apt install build-essential -y
@@ -27,5 +29,3 @@ wget https://raw.githubusercontent.com/tig3rmast3r/vampnet/ismir/scripts/utils/c
 conda list >> new
 python compare.py
 pip install -r requirements_to_update.txt
-conda init bash
-source ~/.bashrc
