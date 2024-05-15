890bdb68d4c52e069bb184039fd841685f21e7bb
tig3rmast3r <tigerman.email@gmail.com>
2024-05-15 21:08:57 +0200

    re-added specific_step arg

--- scripts/utils/train_calculator.py
+++ scripts/utils/train_calculator.py
@@ -8,8 +8,8 @@ def noam_learning_rate(step, d_model, warmup_steps, factor):
     return scale * min(warmup_factor, decay_factor)
 
 def main():
-    if len(sys.argv) != 6:
-        print("Usage: python noamcalculator.py <warmup_steps> <total_steps> <batch_size> <d_model> <factor>")
+    if len(sys.argv) != 7:
+        print("Usage: python train_calculator.py <warmup_steps> <total_steps> <batch_size> <d_model> <factor> <specific_step>")
         return
 
     try:
@@ -18,12 +18,14 @@ def main():
         batch_size = int(sys.argv[3])
         d_model = int(sys.argv[4])
         factor = float(sys.argv[5])
+        specific_step = int(sys.argv[6])
     except ValueError:
-        print("Error: All inputs must be numbers, with batch_size, warmup_steps, total_steps, and d_model as integers.")
+        print("Error: All inputs must be numbers, with batch_size, warmup_steps, total_steps, speficic_step and d_model as integers.")
         return
 
     # Calculate the learning rate at the end of warmup and at a specific step
     max_lr = noam_learning_rate(min(warmup_steps, total_steps), d_model, warmup_steps, factor)
+    specific_lr = noam_learning_rate(specific_step, d_model, warmup_steps, factor)
 
     # Calculate the total sum of learning rates during all iterations
     total_lr_sum = sum(noam_learning_rate(step, d_model, warmup_steps, factor) for step in range(1, total_steps + 1))
@@ -33,6 +35,7 @@ def main():
 
     # Print the results
     print(f"Max learning rate at warmup ({warmup_steps} steps): {max_lr}")
+    print(f"Learning rate at specific step ({specific_step}): {specific_lr}")
     print(f"Total cumulative learning rate for batch size {batch_size} during training ({total_steps} steps): {total_lr_sum_adjusted}")
 
 if __name__ == "__main__":
